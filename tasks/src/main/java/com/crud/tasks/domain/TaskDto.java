4cf1375ee7c48791b77a67ff6289e795e1947f94
LolaTomA <tomlichoraj@gmail.com>
2020-04-29 18:15:12 +0200

    Zadanie 19.2 i 19.3

--- tasks/src/main/java/com/crud/tasks/domain/TaskDto.java
+++ tasks/src/main/java/com/crud/tasks/domain/TaskDto.java
@@ -3,10 +3,11 @@ package com.crud.tasks.domain;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
+@Getter
 public class TaskDto {
     private Long id;
     private String title;
     private String content;
 }
+
